54d8bf62999757998f93e9b49f8c68b0bfb28ee2
Franchesca Zegarra <129009072+Khrisefzm@users.noreply.github.com>
2023-07-26 17:02:15 +0000

    create instagram model

--- src/models.py
+++ src/models.py
@@ -7,23 +7,39 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, primary_key=True)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String, nullable=False )
 
-class Address(Base):
-    __tablename__ = 'address'
+class Follower(Base):
+    __tablename__ = 'follower'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    follower_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+    user = relationship(User)
+
+class Post(Base):
+    __tablename__ = 'post'
+    post_id = Column(Integer, primary_key=True)
+    image_url = Column(String, nullable=False)
+    description = Column(String)
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+    user = relationship(User)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    comment_id = Column(Integer, primary_key=True)
+    description = Column(String)
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+    user = relationship(User)
+    post_id = Column(Integer, ForeignKey('post.post_id'))
+    user = relationship(Post)
 
     def to_dict(self):
         return {}
